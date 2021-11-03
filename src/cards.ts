33f5da8ac73508dd27a62a2c42130ba022497a71
Paul Campbell <paulcampbell@Pauls-MacBook-Air.local>
2021-11-03 19:13:17 +0000

    playing with touch events

--- src/cards.ts
+++ src/cards.ts
@@ -1,15 +1,42 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {eventOptions} from 'lit/decorators.js';
 
 @customElement('my-cards')
 class Cards extends LitElement {
+
   @property()
-  version = 'STARTING';
+  cards = ['card 1', 'card 2', 'card 3'];
+
+  @property()
+  activeCard = this.cards[0];
+ 
+  constructor() {
+    super();
+  }
 
   render() {
     return html`
-    <p>Welcome to the Lit!</p>
-    <p>This is the ${this.version} code.</p>
+    <p>Draggable Cards</p>
+    <ul>
+        ${this.cards.map(
+          (item) =>
+            html`
+              <li draggable="true" @touchmove="${this._handleTouchMove}" @touchstart="${this._handleTouchStart}">${item}</li>
+            `
+        )}
+      </ul>
     `;
+  }
+  private _handleTouchStart(e: Event) { console.log(e) }
+
+  private _handleTouchMove(e: TouchEvent) {
+    const touch = e.touches[0]
+    console.log(touch);
+  }
+
+  private _handleTouchEnd(e: TouchEvent) {
+    const touch = e.touches[0]
+    console.log(touch);
   }
 }
