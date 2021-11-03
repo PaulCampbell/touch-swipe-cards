10f4b39ef7d4cc87a9ea3da1eaedeb5ab0a877cb
Paul Campbell <paulcampbell@Pauls-MacBook-Air.local>
2021-11-03 21:56:37 +0000

    add readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Draggable cards lit web component
+
+
+## Development
+
+### Build:
+
+`npm run watch`
+
+### Serve:
+
+`npm run serve`
--- src/cards.ts
+++ src/cards.ts
@@ -1,6 +1,5 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {eventOptions} from 'lit/decorators.js';
 
 @customElement('my-cards')
 class Cards extends LitElement {
@@ -10,6 +9,53 @@ class Cards extends LitElement {
 
   @property()
   activeCard = this.cards[0];
+
+  @property()
+  dragActive = false;
+
+  @property()
+  activeCardPosition = {}
+
+  static styles = css`
+  ol {
+    padding: 0;
+    margin: 0;
+    -webkit-user-select: none;
+    position: relative;
+    width: 300px;
+    height: 300px;
+  }
+  ol li {
+    list-style: none;
+    background-color: white;
+    padding: 0;
+    margin: 0;
+    border: solid 1px #dedede;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    font-size: 100px;
+    color: white;
+    text-align: center;
+
+  }
+  ol li.active {
+    z-index: 1;
+    padding: 0;
+    margin: 0;
+    border-color: red;
+    position: absolute;
+    box-shadow: 2px 4px 5px 0px rgba(0, 0, 0, 0.23);
+  }
+  
+  ol li.drag {
+    
+  }
+  
+  ol li h2 {
+    font-size: 15px;
+  }
+  `;
  
   constructor() {
     super();
@@ -18,24 +64,35 @@ class Cards extends LitElement {
   render() {
     return html`
     <p>Draggable Cards</p>
-    <ul>
+    <ol>
         ${this.cards.map(
           (item) =>
             html`
-              <li draggable="true" @touchmove="${this._handleTouchMove}" @touchstart="${this._handleTouchStart}">${item}</li>
+              <li 
+                class="${this.activeCard === item && this.dragActive ? 'active' : ''}"
+                @touchmove="${this._handleTouchMove}" 
+                @touchstart="${this._handleTouchStart}"
+                @touchend="${this._handleTouchEnd}"
+                style="${this.activeCard === item && this.dragActive ? 'position: absolute; left:' + this.activeCardPosition.x + 'px' : ''}">
+                  ${item}
+                </li>
             `
         )}
-      </ul>
+      </ol>
     `;
   }
-  private _handleTouchStart(e: Event) { console.log(e) }
+  private _handleTouchStart(e: Event) { 
+    this.dragActive = true;
+    console.log(e) 
+  }
 
   private _handleTouchMove(e: TouchEvent) {
-    const touch = e.touches[0]
-    console.log(touch);
+    const touch = e.targetTouches[0]
+    this.activeCardPosition = { x: touch.clientX, y: touch.clientY }
   }
 
   private _handleTouchEnd(e: TouchEvent) {
+    this.dragActive = false;
     const touch = e.touches[0]
     console.log(touch);
   }
